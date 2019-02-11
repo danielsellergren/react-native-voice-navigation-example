72f02fad786d54ba7c03dbceb9bd8cd6608c6b27
Daniel Sellergren <danielsellergren@Daniels-Air.fios-router.home>
2019-02-10 20:44:12 -0500

    Start voice recognition and output results

--- App.js
+++ App.js
@@ -1,30 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import Voice from 'react-native-voice';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends React.Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  // Initial state and constructor
+  state = {
+    started: '',
+    results: []
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      started: false,
+      results: []
+    };
+
+    Voice.onSpeechStart = this.onSpeechStart.bind(this);
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults.bind(this);
+  }
+
+  // Lifecycle
+  componentDidMount() {
+    console.log("componentDidMount");
+    this._startRecognizing();
+  }
+
+  // Voice
+  async _startRecognizing(e) {
+    this.setState({
+      results: []
+    });
+
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  // Event handlers
+  onSpeechStart(e) {
+    this.setState({
+      started: true
+    });
+  }
+
+  onSpeechPartialResults(e) {
+    console.log("Partial Results");
+    console.log(e.value);
+    this.setState({
+      results: e.value
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to React Native Voice!</Text>
+        <Text style={styles.stat}>{`Started: ${this.state.started}`}</Text>
+        <Text style={styles.stat}>{`Results: ${this.state.results}`}</Text>
+        <Button
+          onPress={this.startListening}
+          title="Voice"
+          color="#841584"
+          accessibilityLabel="Start Listening"
+        />
       </View>
     );
   }
